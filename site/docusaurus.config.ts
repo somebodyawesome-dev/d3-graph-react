73b331b171580fc68a63446a5404b2e48885b7c6
somebodyawesome-dev <luckynoob2011830@gmail.com>
2024-09-07 23:16:17 +0100

    removed blog from site

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -46,11 +46,7 @@ module.exports = {
           label: "Docs",
           position: "left",
         },
-        {
-          to: "blog",
-          label: "Blog",
-          position: "left",
-        },
+       
         {
           href: "https://github.com/somebodyawesome-dev/d3-graph-react",
           label: "GitHub",
